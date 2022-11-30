8a692aff0368c5e0060f7120ab52f40495086bfd
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-11-30 16:48:30 -0500

    router paths

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Header from './components/Footer';
+import Header from './components/Header';
 import Project from './components/Project';
 import Resume from './components/Resume';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* Router component subject to router */}
+      <Router>
+        <Header />
+        {/* Routes component wrap route component defining elements that are conditionally rendered depending on endpoint */}
+        <Routes>
+          {/* End point and component renders to endpoint */}
+          <Route path="/" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
