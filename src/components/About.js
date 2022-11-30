029975cc5797d25d5a665effe8f445cbaf6dab78
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-11-30 17:02:13 -0500

    css styles to about

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import './components.css';
 
 function About() {
   return (
     <div className="container">
-      <h1>This is the about page</h1>
+      <main id="about">
+        <h1 className="lg-heading">
+          About
+          <span className="text-secondary">Me</span>
+        </h1>
+        <h2 className="sm-heading">Let me tell you a few things...</h2>
+        <div className="about-info">
+          {/* <img
+            src="img/portrait.jpg"
+            alt="Evince Edouard"
+            className="bio-image"
+          /> */}
+
+          <div className="bio">
+            <h3 className="text-secondary">Bio</h3>
+            <p>
+              Hi, my name is Evince (pronounced: 'Ev-ince') Edouard. I am
+              passionate about solving problems and being an awesome team
+              member. I am devoted to learning new skills and technologies
+              whether through formal education or self-taught studies. I seek
+              opportunities that combines the worlds of media creation and tech.
+              I am a self taught music-producer/audio engineer and a healthcare
+              professional as well.{' '}
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
