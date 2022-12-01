98e5a3298cf33b967b478485105d57f9652af1a3
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-11-30 19:52:12 -0500

    project added

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,9 +3,17 @@ import React from 'react';
 function Project() {
   return (
     <div className="container">
-      <h1>My projects page</h1>
+      <div className="projects">
+        <div className="item">
+          <a href="#!">
+            <img src="img/projects/project1.jpg" alt="Project" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
+
+// how to hanle a tags and butting tags
 
 export default Project;
