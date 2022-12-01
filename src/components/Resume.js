6b4ca0a7344c59716ec7197df1f87da79998cdb7
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-11-30 20:05:15 -0500

    resume page update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,12 @@ import React from 'react';
 function Resume() {
   return (
     <div className="container">
-      <h1>Resume page</h1>
+      <div>
+        <span className="text-secondary">My Resume</span>
+        <a href="https://docs.google.com/document/d/e/2PACX-1vRCvOzafHbju8Zl1R2lKWQox-J7994os60_oFNB6olwA9DtaNjkMVPv2wyEKgNZnw/pub">
+          Click Here
+        </a>
+      </div>
     </div>
   );
 }
