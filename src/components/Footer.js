163844658cb6be38eb7e1b26151c7fe9b33465a3
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-05 19:42:08 -0500

    project link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer id="main-footer">
-      <div class="icons">
-        <a href="https://www.linkedin.com/in/evinceedouard/">
-          <i class="fab fa-linkedin fa-2x"></i>
-        </a>
-        <a href="https://github.com/EEdouard28">
-          <i class="fab fa-github fa-2x"></i>
-        </a>
-      </div>
-    </footer>
+    <div className="container">
+      <footer id="main-footer">
+        <div class="icons">
+          <a href="https://www.linkedin.com/in/evinceedouard/">
+            <i class="fab fa-linkedin fa-2x"></i>
+          </a>
+          <a href="https://github.com/EEdouard28">
+            <i class="fab fa-github fa-2x"></i>
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
 
