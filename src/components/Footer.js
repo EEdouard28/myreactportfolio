57df68c9e96b0ff3eba621f99121849302563dba
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-05 19:36:28 -0500

    code cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,16 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="container">
-      <footer id="main-footer">
-        <div class="icons">
-          <a href="https://www.linkedin.com/in/evinceedouard/">
-            <i class="fab fa-linkedin fa-2x"></i>
-          </a>
-          <a href="https://github.com/EEdouard28">
-            <i class="fab fa-github fa-2x"></i>
-          </a>
-          Copyright &copy; 2022
-        </div>
-      </footer>
-    </div>
+    <footer id="main-footer">
+      <div class="icons">
+        <a href="https://www.linkedin.com/in/evinceedouard/">
+          <i class="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="https://github.com/EEdouard28">
+          <i class="fab fa-github fa-2x"></i>
+        </a>
+      </div>
+    </footer>
   );
 }
 
