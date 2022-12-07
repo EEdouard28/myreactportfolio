009c151f0c57bf681c3b01f57cb52e13e103596f
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-06 20:47:53 -0500

    contact form build

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contact() {
+  // state variables for name
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    return name === 'firstName' ? setFirstName(value) : setLastName(value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    alert(`Hello ${firstName} ${lastName}`);
+    setFirstName('');
+    setLastName('');
+  };
+
   return (
     <div className="container">
+      <div className="contactForm">
+        {/* Contact Form */}
+        <p>Name area</p>
+        <form className="form">
+          <input
+            value={firstName}
+            name="firstName"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="First Name"
+          />
+
+          <input
+            value="lastName"
+            name="lastName"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Last Name"
+          />
+          {/* <input
+          value="email"
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Please Provide A Email Address"
+          />
+           <input
+          value=
+          name="message"
+          onChange
+          type="text"
+          placeholder="Write A Message"
+          /> */}
+          <button type="button" onClick={handleFormSubmit}>
+            Submit
+          </button>
+        </form>
+      </div>
+
+      {/* Contact Links */}
       <main id="contact">
-        <h1 className="lg-heading">
-          Contact
-          <span className="text-secondary">Me</span>
-        </h1>
         <h2 className="sm-heading">This is how you can reach me...</h2>
         <div className="boxes">
           <div>
